c72a57359ae4ff4b4168814eb5c9d011c2a7ef36
yfor <377900058@qq.com>
2017-03-13 23:19:35 +0800

    条件过滤
    条件过滤

--- business/home/productsList/productsList.js
+++ business/home/productsList/productsList.js
@@ -3,7 +3,7 @@ define(["amaze","framework/services/productService"],function (amaze,productServ
 		var ps = new productService($q);
 		$scope.getProductList=function(){
 			
-			ps.getProductList().then(function(data){
+			ps.getProductList($scope.query).then(function(data){
 				$scope.tipMessageOnLeft="查询产品列表";
 				if(data.code===0){
 					$scope.productList=data.data.products;
@@ -49,7 +49,40 @@ define(["amaze","framework/services/productService"],function (amaze,productServ
 		2:"广告商品",
 		3:"团购商品",
 		4:"秒杀商品"
-	}	
+	}
+	$scope.productCategoriesType=[
+	{
+		name:"单品精选",
+		id:1
+	},
+		{
+		name:"果切",
+		id:2
+	},	{
+		name:"企业套餐",
+		id:3
+	},	{
+		name:"周边",
+		id:4
+	}
+	]
+	$scope.productPropertiesType=[
+	{
+		name:"普通商品",
+		id:1
+	},
+		{
+		name:"广告商品",
+		id:2
+	},
+	{
+		name:"团购商品",
+		id:3
+	},
+		{
+		name:"秒杀商品",
+		id:4
+	}]	
 	}
 	];
 	return ctrl;
